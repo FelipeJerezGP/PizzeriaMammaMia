4b67f5b73448d56632c20b4439df93210a9c8c57
FelipeJerezGP <felipe.jerez@grupoprogestion.com>
2025-08-24 19:12:04 -0400

    Hito uno Pizzeria Mamma Mia

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
